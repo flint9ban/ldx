3440f2cea4710c52663900acb384d43572e19998
liyi <liyi.9ban@gmail.com>
2016-02-22 18:11:36 +0800

    Rejected commit: Default

---
+++ src/main/java/com/gp/persistence/entity/Mountain.java
@@ -0,0 +1,13 @@
+package com.gp.persistence.entity;
+
+/**
+ * Created by liyi on 2016/2/22.
+ */
+import lombok.Data;
+
+@Data
+public class Mountain {
+    private String userName;
+
+    private  String password;
+}
